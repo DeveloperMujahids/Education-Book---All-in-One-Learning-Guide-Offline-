fe2d6b6739b03e03257025984de71dd0e85ac6a1
DeveloperMujahids <mujahid247866@gmail.com>
2024-02-08 08:49:16 -0800

    education book

---
+++ app/src/main/java/com/mujahid/computereducation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mujahid.computereducation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+    /////=============================================
+}
